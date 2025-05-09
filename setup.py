4351de56d0d46dd7bc46f594233c5413fdfcb82a
Aryan Palaskar <aryan.palaskar3@gmail.com>
2025-05-09 14:02:09 -0700

    Added Qt5Agg backend for MatplotLib for better performance

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ setup(
         "tk",
         "bezier",
         "importlib_resources",
+        "PyQt5>=5.6",
     ],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
